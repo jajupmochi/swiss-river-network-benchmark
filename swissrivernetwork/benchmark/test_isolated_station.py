feat: apply day-wise pooling / aggregation; revise training loop to fix valid mse computation bug and report more metrics;

swissrivernetwork/benchmark/test_isolated_station.py
@@ -4,6 +4,7 @@ from ray.tune import ExperimentAnalysis
 from swissrivernetwork.benchmark.dataset import *
 from swissrivernetwork.benchmark.model import LstmModel, LstmEmbeddingModel
 from swissrivernetwork.benchmark.util import *
+from swissrivernetwork.experiment.error import compute_errors
 
 SHOW_PLOT = False
 
@@ -110,11 +111,10 @@ def run_transformer_model(
             # mask values:
             mask = ~torch.isnan(y)
             masks.append(mask[0])
-            if mask.sum() == 0:
-                continue  # skip if all is masked
+            # if mask.sum() == 0:
+            #     continue  # skip if all is masked
 
-            y = y[mask]
-            out = out[mask]
+            # Store everything and mask later, so that we can aggregate later:
             out = normalizer_wt.inverse_transform(out.detach().numpy().reshape(-1, 1))
 
             # Store values
@@ -127,6 +127,23 @@ def run_transformer_model(
     masks = np.concatenate(masks, axis=0).flatten()
     actual = np.concatenate(actual, axis=0).flatten()
     prediction = np.concatenate(prediction, axis=0).flatten()
+
+    # Notice that `last` or `longest_history` do not work if dataloader is shuffled!
+    unique_epoch_days, aggregated_dict = aggregate_day_predictions(
+        epoch_days,
+        {'prediction_norm': prediction_norm, 'mask': masks, 'actual': actual, 'prediction': prediction},
+        method='longest_history'
+    )
+    # Store epoch_days, prediction_norm and masks on all days:
+    epoch_days = unique_epoch_days
+    prediction_norm = aggregated_dict['prediction_norm']
+    masks = aggregated_dict['mask']
+    # Apply mask on actual and prediction:
+    actual = aggregated_dict['actual']
+    prediction = aggregated_dict['prediction']
+    actual = actual[masks]
+    prediction = prediction[masks]
+
     return epoch_days, prediction_norm, masks, actual, prediction
 
 
@@ -151,14 +168,6 @@ def fit_normalizers(df):
     normalizer_at = MinMaxScaler().fit(df['air_temperature'].values.reshape(-1, 1))
     normalizer_wt = MinMaxScaler().fit(df['water_temperature'].values.reshape(-1, 1))
     return normalizer_at, normalizer_wt
-
-
-def compute_errors(actual, prediction):
-    from swissrivernetwork.experiment.error import Error
-    rmse = Error.rmse(actual, prediction)
-    mae = Error.mae(actual, prediction)
-    nse = Error.nse(actual, prediction)
-    return rmse, mae, nse
 
 
 def summary(station, rmse, mae, nse):
@@ -305,7 +314,7 @@ def test_lstm_embedding(
     # Plot Figure of Test Data
     plot(
         graph_name, 'lstm_embedding', station, epoch_days[mask], actual, prediction, title,
-        plot_diff=True,  # fixme: debug
+        plot_diff=True,  # debug
         dump_dir=dump_dir
     )
 
@@ -332,9 +341,13 @@ def test_transformer_embedding(
     print(title)
 
     # Plot Figure of Test Data
-    plot(graph_name, 'transformer_embedding', station, epoch_days[mask], actual, prediction, title, dump_dir=dump_dir)
+    plot(
+        graph_name, 'transformer_embedding', station, epoch_days[mask], actual, prediction, title,
+        plot_diff=True,  # debug
+        dump_dir=dump_dir
+    )
 
-    return rmse, mae, nse, len(prediction)
+    return rmse, mae, nse, len(prediction), (actual, prediction, epoch_days[mask])
 
 
 if __name__ == '__main__':

swissrivernetwork/benchmark/train_single_model.py
@@ -20,7 +20,7 @@ def train_lstm_embedding(config, settings: benedict = benedict({}), verbose: int
     datasets_valid = []
     for i, station in enumerate(stations):
         df_station = select_isolated_station(df, station)
-        dataset_train, dataset_valid = create_dataset_embedding(config, df_station, i)
+        dataset_train, dataset_valid, normalizer_at, normalizer_wt = create_dataset_embedding(config, df_station, i)
         datasets_train.append(dataset_train)
         datasets_valid.append(dataset_valid)
     dataset_train = torch.utils.data.ConcatDataset(datasets_train)
@@ -36,13 +36,13 @@ def train_lstm_embedding(config, settings: benedict = benedict({}), verbose: int
     # Run Training Loop!
     training_loop(
         config, dataloader_train, dataloader_valid, model, len(dataset_valid), use_embedding=True,
-        settings=settings, verbose=verbose
+        normalizer_at=normalizer_at, normalizer_wt=normalizer_wt, settings=settings, verbose=verbose
     )
 
 
 def create_dataset_embedding(config, df, i, valid_use_window: bool = False):
     # Normalize
-    df = normalize_isolated_station(df)
+    df, normalizer_at, normalizer_wt = normalize_isolated_station(df)
 
     # Train/Validation split
     df_train, df_valid = train_valid_split(config, df)
@@ -52,10 +52,12 @@ def create_dataset_embedding(config, df, i, valid_use_window: bool = False):
         config['window_len'], df_train, embedding_idx=i, dev_run=config.get('dev_run', False)
     )
     if valid_use_window:
-        dataset_valid = SequenceWindowedDataset(config['window_len'], df_valid, embedding_idx=i)
+        dataset_valid = SequenceWindowedDataset(
+            config['window_len'], df_valid, embedding_idx=i, dev_run=config.get('dev_run', False)
+        )
     else:
         dataset_valid = SequenceFullDataset(df_valid, embedding_idx=i)
-    return dataset_train, dataset_valid
+    return dataset_train, dataset_valid, normalizer_at, normalizer_wt
 
 
 def train_stgnn(config, settings: benedict = benedict({}), verbose: int = 2):
@@ -107,7 +109,9 @@ def train_transformer(config, settings: benedict = benedict({}), verbose: int = 
     datasets_valid = []
     for i, station in enumerate(stations):
         df_station = select_isolated_station(df, station)
-        dataset_train, dataset_valid = create_dataset_embedding(config, df_station, i, valid_use_window=True)
+        dataset_train, dataset_valid, normalizer_at, normalizer_wt = create_dataset_embedding(
+            config, df_station, i, valid_use_window=True
+        )
         datasets_train.append(dataset_train)
         datasets_valid.append(dataset_valid)
     dataset_train = torch.utils.data.ConcatDataset(datasets_train)
@@ -132,8 +136,8 @@ def train_transformer(config, settings: benedict = benedict({}), verbose: int = 
 
     # Run Training Loop!
     training_loop(
-        config, dataloader_train, dataloader_valid, model, len(dataset_valid), use_embedding=True, settings=settings,
-        verbose=verbose
+        config, dataloader_train, dataloader_valid, model, len(dataset_valid), use_embedding=True,
+        normalizer_at=normalizer_at, normalizer_wt=normalizer_wt, settings=settings, verbose=verbose
     )
 
 
@@ -164,12 +168,12 @@ if __name__ == '__main__':
     # Extra config:
     config.update(
         {
-            'dev_run': False,  # fixme: debug
+            'dev_run': False,  # fixme: debug  Set training and validation to very small subsets (4) and disable wandb
         }
     )
 
-    # # train_lstm_embedding(config)
-    # train_stgnn(config)
+    # train_lstm_embedding(config, settings=benedict({'enable_wandb': True, 'method': 'lstm_embedding'}))
+    # # # train_stgnn(config)
 
     # Transformer specific:
     config.update(
@@ -182,4 +186,4 @@ if __name__ == '__main__':
             'use_station_embedding': True
         }
     )
-    train_transformer(config, settings=benedict({'enable_wandb': True}))
+    train_transformer(config, settings=benedict({'enable_wandb': True, 'method': 'transformer_embedding'}))

swissrivernetwork/benchmark/training.py
@@ -1,6 +1,7 @@
 import sys
 import tempfile
 
+import numpy as np
 import torch
 import torch.nn as nn
 import wandb
@@ -10,7 +11,8 @@ from ray.tune import Checkpoint, report
 from torchinfo import summary
 from tqdm import tqdm
 
-from swissrivernetwork.benchmark.util import save
+from swissrivernetwork.benchmark.util import save, aggregate_day_predictions
+from swissrivernetwork.experiment.error import Error
 
 ISSUE_TAG = "\033[91m[issue]\033[0m "  # Red
 INFO_TAG = "\033[94m[info]\033[0m "  # Blue
@@ -19,6 +21,7 @@ SUCCESS_TAG = "\033[92m[success]\033[0m "  # Green
 
 def training_loop(
         config, dataloader_train, dataloader_valid, model, n_valid, use_embedding, edges=None,
+        normalizer_at: object = None, normalizer_wt: object = None,
         wandb_project: str | None = 'swissrivernetwork', settings: benedict = benedict({}),
         verbose: int = 2
 ):
@@ -57,9 +60,11 @@ def training_loop(
         # Run the Training loop on the Model
         optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
         criterion = nn.MSELoss()
-        validation_criterion = nn.MSELoss(reduction='sum')  # weight all samples equally
+        validation_criterion = nn.MSELoss(reduction='mean')  # weight all samples equally
 
         for epoch in range(config['epochs']):
+            metrics_to_report = {}
+
             model.train()
             losses = []
 
@@ -76,6 +81,11 @@ def training_loop(
             else:
                 iterator = dataloader_train
 
+            # The training loss MSE is computed for each sub-sequence, then averaged over all sub-sequences.
+            # When window_len remains the same and no masking is applied, this is equivalent to computing
+            # the MSE over all samples.
+            # Notice that shuffling is applied for training, so the order of samples is different from
+            # the original time order.
             for step, (_, e, x, y) in enumerate(iterator):
                 e, x, y = e.to(device), x.to(device), y.to(device)
                 optimizer.zero_grad()
@@ -96,15 +106,24 @@ def training_loop(
                 # halo.update(step, loss.item())
 
             train_loss = sum(losses) / len(losses)
+            metrics_to_report['train_loss'] = train_loss
 
             # halo.succeed(f'Epoch {epoch + 1}: Avg Loss = {sum(losses) / len(losses):.4f}.')
-            print(f'Epoch {epoch + 1}: Avg Loss = {train_loss:.4f}.')
+            # print(f'Epoch {epoch + 1}: Avg Train Loss = {train_loss:.4f}.')
 
             model.eval()
-            validation_mse = 0
+            epoch_days, preds, targets, masks = [], [], [], []
             with torch.no_grad():
-                for _, e, x, y in dataloader_valid:
-                    e, x, y = e.to(device), x.to(device), y.to(device)
+                if verbose >= 2:
+                    iterator_valid = tqdm(
+                        dataloader_valid, desc=f'  Validation', file=sys.stdout,
+                        colour='blue'
+                    )
+                else:
+                    iterator_valid = dataloader_valid
+
+                for t, e, x, y in iterator_valid:
+                    t, e, x, y = t.to(device), e.to(device), x.to(device), y.to(device)
                     if edges is not None:
                         out = model(x, edges)
                     elif use_embedding:
@@ -112,24 +131,132 @@ def training_loop(
                     else:
                         out = model(x)
                     mask = ~torch.isnan(y)  # mask NaNs
-                    loss = validation_criterion(out[mask], y[mask])
-                    validation_mse += loss.item()
-            validation_mse /= n_valid  # normalize by dataset length
+                    # Record everything (not masked) for possible aggregation later:
+                    epoch_days.append(t[0])
+                    masks.append(mask[0])
+                    preds.append(out[0])  # Both preds and targets are normalized
+                    targets.append(y[0])
+
+            # preds, targets, masks, etc. are lists of tensors in shape [sub_seq_len, 1].
+            # For SequenceFullDataset, each tensor corresponds to one station;
+            # For SequenceWindowedDataset, each tensor corresponds to one sub-sequence of a station.
+            # - For valid loss, we concatenate all samples and compute the total mse over all samples. **It is the same as
+            #   the training loss** when window_len keeps unchanged and no masking is applied for training.
+            #   Notice this may favor stations with more samples.
+            # - For valid evaluation, we compute two metrics:
+            #   (1) RMSE over all samples (**same as valid loss**).
+            #   (2) Averaged RMSE over stations (to treat each station equally). For SequenceWindowedDataset,
+            #       this requires to aggregate predictions for each station first, e.g., by the 'longest_history' method,
+            #       so that each day has only one prediction per station, the same as using SequenceFullDataset.
+            #       Then compute the RMSE for each station, and finally average the RMSEs over all stations.
+            #       **This is the same metric as in Ray tuner evaluation for test sets**.
+            all_masks, all_preds, all_targets, all_preds_norm, all_targets_norm = [], [], [], [], []
+            valid_ave_rmses = []
+            cumulative_sizes = [0] + dataloader_valid.dataset.cumulative_sizes  # cumulat # of sub-sequences per station
+            for start, end in zip(cumulative_sizes[:-1], cumulative_sizes[1:]):
+                station_epoch_days = torch.cat(epoch_days[start:end], dim=0).flatten()
+                station_masks = torch.cat(masks[start:end], dim=0).flatten()
+                station_preds_norm = torch.cat(preds[start:end], dim=0).flatten()
+                station_targets_norm = torch.cat(targets[start:end], dim=0).flatten()
+                # todo: upgrade with aggregation method in config or full/windowed dataset
+                if settings.method in ['transformer_embedding']:
+                    unique_epoch_days, aggregated_dict = aggregate_day_predictions(
+                        station_epoch_days,
+                        {
+                            'masks': station_masks, 'preds_norm': station_preds_norm,
+                            'targets_norm': station_targets_norm
+                        },
+                        method='longest_history'
+                    )
+                    station_masks = aggregated_dict['masks']
+                    station_preds_norm = aggregated_dict['preds_norm'][station_masks]  # masked tensor
+                    station_targets_norm = aggregated_dict['targets_norm'][station_masks]  # masked tensor
+                else:
+                    station_preds_norm = station_preds_norm[station_masks]
+                    station_targets_norm = station_targets_norm[station_masks]
+
+                station_preds = normalizer_wt.inverse_transform(
+                    station_preds_norm.cpu().numpy().reshape(-1, 1)
+                ).flatten()  # masked array
+                station_targets = normalizer_wt.inverse_transform(
+                    station_targets_norm.cpu().numpy().reshape(-1, 1)
+                ).flatten()  # masked array
+
+                valid_ave_rmse = Error.rmse(station_preds, station_targets)
+                valid_ave_rmses.append(valid_ave_rmse)
+
+                all_preds_norm.append(station_preds_norm)
+                all_targets_norm.append(station_targets_norm)
+                all_masks.append(station_masks.cpu().numpy())  # array
+                all_preds.append(station_preds)
+                all_targets.append(station_targets)
+
+            all_preds_norm = torch.concatenate(all_preds_norm, dim=0)
+            all_targets_norm = torch.concatenate(all_targets_norm, dim=0)
+            all_masks = np.concatenate(all_masks, axis=0)
+            all_preds = np.concatenate(all_preds, axis=0)
+            all_targets = np.concatenate(all_targets, axis=0)
+            n_valid = np.sum(all_masks)
+            if n_valid == 0:
+                print(f'{ISSUE_TAG}Warning: No valid samples in validation set after masking NaNs!')
+                raise StopIteration
+
+            validation_mse = validation_criterion(all_preds_norm, all_targets_norm).cpu().numpy()
+            validation_rmse = Error.rmse(all_preds, all_targets)
+            validation_ave_rmse = np.mean(valid_ave_rmses)
+
+            # # This is another way to compute the same metrics for SequenceFullDataset: (needs repair)
+            # all_masks = torch.cat(masks, dim=0).flatten()  # `dim` considers first dim as batch dim
+            # all_preds_norm = torch.cat(preds, dim=0).flatten()
+            # all_targets_norm = torch.cat(targets, dim=0).flatten()
+            # n_valid = all_masks.sum().item()
+            # if n_valid == 0:
+            #     print(f'{ISSUE_TAG}Warning: No valid samples in validation set after masking NaNs!')
+            #     raise StopIteration
+            # validation_mse = validation_criterion(all_preds_norm[all_masks], all_targets_norm[all_masks]).cpu().numpy()
+            #
+            # all_preds = normalizer_wt.inverse_transform(all_preds_norm[all_masks].cpu().numpy().reshape(-1, 1).flatten())
+            # all_targets = normalizer_wt.inverse_transform(all_targets_norm[all_masks].cpu().numpy().reshape(-1, 1).flatten())
+            # validation_rmse = Error.rmse(all_preds, all_targets)
+            # valid_ave_rmses = []
+            #
+            # cumulative_sizes = [0] + dataloader_valid.dataset.cumulative_sizes  # cumulat # of sub-sequences per station
+            # for start, end in zip(cumulative_sizes[:-1], cumulative_sizes[1:]):
+            #     station_epoch_days = torch.cat(epoch_days[start:end], dim=0).flatten()
+            #     station_masks = torch.cat(masks[start:end], dim=0).flatten()
+            #     station_preds = torch.cat(preds[start:end], dim=0).flatten()
+            #     station_targets = torch.cat(targets[start:end], dim=0).flatten()
+            #     valid_ave_rmse = Error.rmse(
+            #         station_preds[station_masks].cpu().numpy(), station_targets[station_masks].cpu().numpy()
+            #     )
+            #     valid_ave_rmses.append(valid_ave_rmse)
+            # validation_ave_rmse = np.mean(valid_ave_rmses)
+
+            print(f'{INFO_TAG}cumulative_sizes: {cumulative_sizes}')
+            print(f'{INFO_TAG}len(validation_ave_rmses): {len(valid_ave_rmses)}')
+            print(f'{INFO_TAG}n_valid: {n_valid}')
+
+            # Log everything:
+
+            metrics_to_report['validation_mse'] = validation_mse
+            metrics_to_report['validation_ave_rmse'] = validation_ave_rmse
+            metrics_to_report['validation_rmse'] = validation_rmse
 
             # Register Ray Checkpoint
             checkpoint_dir = tempfile.mkdtemp()
-            save(model.state_dict(), checkpoint_dir, f'lstm_epoch_{epoch + 1}.pth')
+            save(model.state_dict(), checkpoint_dir, f'{settings.get("method", "model_epoch")}_{epoch + 1}.pth')
             # save(normalizer_at, checkpoint_dir, 'normalizer_at.pth')
             # save(normalizer_wt, checkpoint_dir, 'normalizer_wt.pth')
             checkpoint = Checkpoint.from_directory(checkpoint_dir)
 
             # report epoch loss
-            report({"train_loss": train_loss, "validation_mse": validation_mse}, checkpoint=checkpoint)
-            print(f'End of Epoch {epoch + 1}: {validation_mse:.5f}')
+            report(metrics_to_report, checkpoint=checkpoint)
 
-            wandb.log({'epoch': epoch + 1, 'train_loss': train_loss})
-            wandb.log({'epoch': epoch + 1, 'valid_loss': validation_mse})
-            wandb.log({'epoch': epoch + 1, 'validation_mse': validation_mse})
+            for k, v in metrics_to_report.items():
+                wandb.log({'epoch': epoch + 1, k: v})
+
+            metric_str = ', '.join([f'{k} = {v:.5f}' for k, v in metrics_to_report.items()])
+            print(f'{INFO_TAG}End of Epoch {epoch + 1}: {metric_str}.')
 
     except RuntimeError as e:
         if "out of memory" in str(e).lower():

swissrivernetwork/benchmark/util.py
@@ -1,34 +1,149 @@
 import os
-import torch
+
+import numpy as np
 import pandas as pd
+import torch
+from torch_geometric.utils import k_hop_subgraph, to_undirected
 
 from swissrivernetwork.benchmark.dataset import read_graph
-from torch_geometric.utils import k_hop_subgraph, to_undirected
+
 
 def save(object, checkpoint_dir, name):
     path = os.path.join(checkpoint_dir, name)
     torch.save(object, path)
 
+
 def extract_neighbors(graph_name, station, num_hops):
     # Use undirected edges:
     x, e = read_graph(graph_name)
-    e = to_undirected(e)    
+    e = to_undirected(e)
 
     # Extract k-hop neighborhood
     idx = (x[:, 2] == int(station)).nonzero().item()
     x_sub_idx, e, _, _ = k_hop_subgraph(idx, num_hops, e, relabel_nodes=True)
     x = x[x_sub_idx]
     neighs = [str(s.item()) for s in x[:, 2]]
-    neighs.remove(station) # remove target station 
+    neighs.remove(station)  # remove target station
     return neighs
 
+
 def merge_graphlet_dfs(df, df_neighs):
-    for df_neigh in df_neighs:        
+    for df_neigh in df_neighs:
         df = pd.merge(df, df_neigh, on='epoch_day', how='outer')
         # fill NaN:
         col = df_neigh.columns[1]
-        #df[col] = df[col].fillna(-1)
+        # df[col] = df[col].fillna(-1)
         assert df[col].isna().sum() == 0, f'NaN in neigh {col} detected!'
     has_any_nan = df.drop(columns=['air_temperature', 'water_temperature']).isna().any().any()
     assert not has_any_nan, 'There is a NaN in your data!'
     return df
+
+
+def aggregate_day_predictions(
+        epoch_days: np.ndarray | torch.Tensor,
+        dict_to_aggregate: dict[str, np.ndarray | torch.Tensor], method: str | None = 'longest_history'
+) -> (np.ndarray | torch.Tensor, dict[str, np.ndarray | torch.Tensor]):
+    """
+    Aggregate day predictions from multiple windows.
+    Input:
+        epoch_days: np.ndarray or torch.Tensor of shape (total_length,). Make sure that the days are in time order if using
+            'longest_history' or 'last' methods.
+        dict_to_aggregate: dict of np.ndarray or torch.Tensor of shape (total_length,). Each array must align with `epoch_days`.
+        method: aggregation method, one of [None, 'last', 'mean', 'median', 'longest_history']. Default: 'longest_history'.
+    Output:
+        unique_epoch_days: np.ndarray or torch.Tensor of shape (num_unique_days,)
+        aggregated_dict: dict of np.ndarray or torch.Tensor of shape (num_unique_days, ...)
+    """
+    if method is None:
+        return epoch_days, dict_to_aggregate
+
+    if method == 'longest_history':
+        # For each given day, this method chooses the predictions for that day who has the longest history.
+        # Here we assume that the input `epoch_days` is organized as the concatenation of multiple sliding windows,
+        # (e.g., the dataloader with batch_size=1 and shuffle=False). Thus, for each day, the first occurrence of that
+        # day in `epoch_days` corresponds to the window with the longest history.
+        if isinstance(epoch_days, torch.Tensor):
+            unique_epoch_days, day_indices = torch_unique_as_numpy(epoch_days, return_index=True)
+            aggregated_dict = {}
+            for key, values in dict_to_aggregate.items():
+                aggregated_dict[key] = values[day_indices]
+            return unique_epoch_days, aggregated_dict
+        else:
+            unique_epoch_days, day_indices = np.unique(epoch_days, return_index=True)
+            aggregated_dict = {}
+            for key, values in dict_to_aggregate.items():
+                aggregated_dict[key] = values[day_indices]
+            return unique_epoch_days, aggregated_dict
+
+    elif method == 'last':
+        # Here "last" means that for the prediction of a given day, we take the prediction from the last window:
+        if isinstance(epoch_days, torch.Tensor):
+            raise NotImplementedError('last method not implemented for torch.Tensor yet.')
+        else:
+            unique_epoch_days, idx = np.unique(epoch_days[::-1], return_index=True)
+            unique_epoch_days = unique_epoch_days[::-1]
+            last_indices = len(epoch_days) - 1 - idx[::-1]
+            aggregated_dict = {}
+            for key, values in dict_to_aggregate.items():
+                aggregated_dict[key] = values[last_indices]
+            return unique_epoch_days, aggregated_dict
+
+    elif method == 'mean':
+        if isinstance(epoch_days, torch.Tensor):
+            raise NotImplementedError('mean method not implemented for torch.Tensor yet.')
+        else:
+            unique_epoch_days = np.unique(epoch_days)
+            aggregated_dict = {}
+            for key, values in dict_to_aggregate.items():  # todo: use np.bincount for speed
+                agg_values = []
+                for day in unique_epoch_days:
+                    day_values = values[epoch_days == day]
+                    agg_values.append(np.mean(day_values, axis=0))
+                aggregated_dict[key] = np.array(agg_values)
+            return unique_epoch_days, aggregated_dict
+
+    elif method == 'median':
+        if isinstance(epoch_days, torch.Tensor):
+            raise NotImplementedError('median method not implemented for torch.Tensor yet.')
+        else:
+            unique_epoch_days = np.unique(epoch_days)
+            aggregated_dict = {}
+            for key, values in dict_to_aggregate.items():
+                agg_values = []
+                for day in unique_epoch_days:
+                    day_values = values[epoch_days == day]
+                    agg_values.append(np.median(day_values, axis=0))
+                aggregated_dict[key] = np.array(agg_values)
+            return unique_epoch_days, aggregated_dict
+
+    else:
+        raise ValueError(f'Unknown aggregation method: {method}.')
+
+
+def torch_unique_as_numpy(t: torch.Tensor, return_index: bool = False) -> torch.Tensor:
+    """
+    Mimic the behavior of `np.unique` for torch.Tensor.
+    This function returns the unique values in the order of their first occurrence in the input tensor. The unique values
+    are sorted in ascending order by default.
+
+    Notice that `torch.unique` always sorts the unique values, while `np.unique` does not sort by default.
+
+    Input:
+        t: torch.Tensor of any shape.
+        return_index: whether to return the indices of the first occurrences of the unique values.
+
+    Output:
+        unique_vals: torch.Tensor of shape (num_unique_vals,)
+        first_indices (optional): torch.Tensor of shape (num_unique_vals,)
+    """
+    # Notice `torch.unique` always sorts no matter to what `sorted` is set.
+    # `reverse_indices` gives the indices to reconstruct the original tensor from the unique tensor:
+    unique_vals, inverse_indices, counts = torch.unique(t, return_inverse=True, return_counts=True)
+    _, ind_sorted = torch.sort(inverse_indices, stable=True)
+    cum_sum = counts.cumsum(0)
+    cum_sum = torch.cat((torch.tensor([0], device=cum_sum.device), cum_sum[:-1]))
+    first_indices = ind_sorted[cum_sum]
+    if return_index:
+        return unique_vals, first_indices
+    else:
+        return unique_vals