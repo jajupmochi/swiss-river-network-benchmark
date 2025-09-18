a30241657e67d0aebe95acb25784ff80c6595588
jajupmochi <jajupmochi@gmail.com>
2025-09-18 18:05:16 +0200

    fix: window lengths error for full sequence dataset.

--- swissrivernetwork/benchmark/dataset.py
+++ swissrivernetwork/benchmark/dataset.py
@@ -1,8 +1,8 @@
-import torch
-import numpy as np
-import pandas as pd
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+import torch
 from sklearn.preprocessing import MinMaxScaler
 
 CUR_ABS_DIR = Path(__file__).parent.resolve()
@@ -53,7 +53,7 @@ def normalize_isolated_station(df):
         assert False, 'We can not handle NaN in Input!'
         # df['air_temperature'] = df['air_temperature'].fillna(-1)
 
-    # NaN in Output will be masekd
+    # NaN in Output will be masked
     normalizer_wt = MinMaxScaler()
     df['water_temperature'] = normalizer_wt.fit_transform(df['water_temperature'].values.reshape(-1, 1))
     if df['water_temperature'].isna().sum() > 0:
@@ -98,7 +98,10 @@ class SequenceDataset(torch.utils.data.Dataset):
         breaks = day_diff != 1
         sequence_id = breaks.cumsum()
         sequences = self.df.index[breaks].values
-        sequence_lengths = sequence_id.value_counts(sort=False).sort_index().values - self.window_len + 1
+        if self.window_len <= 0:
+            sequence_lengths = sequence_id.value_counts(sort=False).sort_index().values
+        else:
+            sequence_lengths = sequence_id.value_counts(sort=False).sort_index().values - self.window_len + 1
 
         # remove short sequences
         drop_sequences = sequence_lengths < 0
@@ -190,6 +193,156 @@ class SequenceWindowedDataset(SequenceDataset):
     def __getitem__(self, idx):
         for i, length in enumerate(self.sequence_lengths):
             # This length already excludes windows that are too short. See `SequenceDataset.extract_sequences()`.
+            if idx >= length:
+                idx -= length
+                continue
+
+            # idx is now in sequence:
+            start = self.sequences[i] + idx
+
+            df = self.df.iloc[start:start + self.window_len]  # Windowed DF
+            return self.as_tensors(df)
+
+
+# %%
+
+
+class SequenceMaskedDataset(SequenceDataset):
+
+    def __init__(self, window_len, df, embedding_idx, max_mask_ratio: float = 0.25, max_mask_consecutive: int = 10):
+        self.max_mask_ratio = max_mask_ratio
+        self.max_mask_consecutive = max_mask_consecutive
+        # self.time_masks = None  # will be set in extract_sequences
+        super().__init__(window_len, df, embedding_idx)
+
+
+    def extract_sequences(self):
+        day_diff = self.df['epoch_day'].diff()
+        if (day_diff < 0).any():
+            raise ValueError('DataFrame must be sorted by epoch_day in ascending order!')
+        if (day_diff[1:].isna()).any():
+            raise ValueError('DataFrame must not contain NaN in ``epoch_day``!')
+
+        large_breaks = (day_diff > self.max_mask_consecutive + 1)
+
+        # Pad with 0 at row with small day gaps and add time_mask column:
+        dtype_dict = {col: self.df[col].dtype for col in self.df.columns}
+        dtype_dict['time_mask'] = bool
+        df_padded = pd.DataFrame(columns=self.df.columns.tolist() + ['time_mask']).astype(dtype_dict)
+        breaks = day_diff != 1  # bool
+        sequence_id = breaks.cumsum()  # int, starting from 1
+        sequences = self.df.index[breaks].values  # idx list
+        sequence_lengths = sequence_id.value_counts(sort=False).sort_index().values  # int list
+        starts = sequences  # idx list
+        ends = sequences + sequence_lengths  # idx list, exclusive
+        for i_seq, (start, end) in enumerate(zip(starts, ends)):
+            df_sequence = self.df.iloc[start:end].copy()
+            df_sequence['time_mask'] = False  # False means we have a value, not a mask
+            df_padded = pd.concat([df_padded, df_sequence], ignore_index=True)
+            if end >= len(self.df):
+                break
+            cur_day_diff = int(self.df.iloc[end]['epoch_day'] - self.df.iloc[end - 1]['epoch_day'])
+            if cur_day_diff <= self.max_mask_consecutive + 1:
+                # missing days to pad (if padding with nan, transformer QK matrix will corrupt even with mask):
+                # todo: check how it affects lstms
+                df_gaps = pd.DataFrame(0, index=range(cur_day_diff - 1), columns=self.df.columns)
+                df_gaps['epoch_day'] = range(
+                    int(self.df.iloc[end - 1]['epoch_day'] + 1), int(self.df.iloc[end]['epoch_day'])
+                )
+                df_gaps['time_mask'] = True  # True means we have a mask / missing value to be ignored
+                df_padded = pd.concat([df_padded, df_gaps], ignore_index=True)
+
+        self.df = df_padded.reset_index(drop=True)
+
+        # Then, rerun the sequence extraction on the padded df:
+        day_diff = self.df['epoch_day'].diff()
+        breaks = day_diff != 1  # bool
+        assert (breaks.sum() == large_breaks.sum() + 1), 'Large breaks mismatch!'
+
+        sequence_id = breaks.cumsum()  # int, starting from 1
+        sequences = self.df.index[breaks].values  # idx list
+        if self.window_len <= 0:
+            sequence_lengths = sequence_id.value_counts(sort=False).sort_index().values
+        else:
+            sequence_lengths = sequence_id.value_counts(sort=False).sort_index().values - self.window_len + 1
+
+        # remove short sequences:
+        drop_sequences = sequence_lengths < 0
+        # todo: Consider max_mask_ratio here
+        # drop_sequences = drop_sequences | (self.masks.groupby(sequence_id).sum().values > self.max_mask_ratio * sequence_lengths)
+        self.sequences = sequences[~drop_sequences]
+        self.sequence_lengths = sequence_lengths[~drop_sequences]
+
+
+    def as_tensors(self, df: pd.DataFrame):
+        t = torch.IntTensor(df['epoch_day'].values).unsqueeze(-1)
+        x = torch.FloatTensor(df['air_temperature'].values).unsqueeze(-1)
+        y = torch.FloatTensor(df['water_temperature'].values).unsqueeze(-1)
+        time_masks = torch.BoolTensor(df['time_mask'].values)  # [seq_len], True means to ignore / to mask
+
+        # check for predictions (wt_hat):
+        matches = df.columns.str.contains(r'_wt_hat$')
+        matching_columns = df.columns[matches]
+        neighs = [torch.FloatTensor(df[col].values).unsqueeze(-1) for col in matching_columns]
+        for neigh in neighs:
+            x = torch.cat((x, neigh), dim=1)
+
+        # check for embedding_idx:
+        embs = torch.zeros((x.shape[0]), dtype=torch.long)
+        if self.embedding_idx is not None:
+            embs = torch.LongTensor([self.embedding_idx] * x.shape[0])
+
+        if t is None or embs is None or x is None or y is None:
+            print('haaaaaalt!')
+
+        return (t, embs, x, y, time_masks)
+
+
+    def as_stgnn_tensors(self, df):
+        ts = []
+        xs = []
+        ys = []
+        for station in self.stations:
+            ts.append(torch.IntTensor(df['epoch_day'].values).unsqueeze(-1))
+            xs.append(torch.FloatTensor(df[f'{station}_at'].values).unsqueeze(-1))
+            ys.append(torch.FloatTensor(df[f'{station}_wt'].values).unsqueeze(-1))
+
+        t = torch.stack(ts, dim=0)
+        x = torch.stack(xs, dim=0)
+        y = torch.stack(ys, dim=0)
+
+        # add empty embeddings
+        embs = torch.zeros((x.shape[0], x.shape[1]), dtype=torch.long)
+
+        if t is None or embs is None or x is None or y is None:
+            print('haaaaaalt!')
+
+        return (t, embs, x, y)
+
+
+class SequenceMaskedWindowedDataset(SequenceMaskedDataset):
+
+    def __init__(
+            self, window_len, df, embedding_idx=None,
+            max_mask_ratio: float = 0.25, max_mask_consecutive: int = 10,
+            dev_run: bool = False
+    ):
+        super().__init__(
+            window_len, df, embedding_idx, max_mask_ratio=max_mask_ratio, max_mask_consecutive=max_mask_consecutive
+        )
+        self.dev_run = dev_run
+
+
+    def __len__(self):
+        if self.dev_run:
+            return min(10, np.sum(self.sequence_lengths))
+        else:
+            return np.sum(self.sequence_lengths)
+
+
+    def __getitem__(self, idx: int):
+        for i, length in enumerate(self.sequence_lengths):
+            # This length already excludes windows that are too short. See `SequenceMaskedDataset.extract_sequences()`.
             if idx >= length:
                 idx -= length
                 continue
